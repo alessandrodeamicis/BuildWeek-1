3ad7642f007300a66afc6c33c37b5e7c8a92b465
reru96 <renatorumieri96@outlook.com>
2025-06-11 20:36:23 +0200

    EnemyWaypoint
    aggiunto al nemico variant2

---
+++ Assets/Scripts/Enemy/EnemyWaypoint.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaypoint : MonoBehaviour
+{
+
+    public Transform waypointParent;
+    public float movespeed = 5f;
+    public float waitTime = 2f;
+    public bool loopWaypoints = true;
+    public float detectionRange = 3f;
+
+    // Start is called before the first frame update
+
+    private Transform[] waypoints;
+    private int currentWaypointIndex;
+    private bool isWaiting;
+    private Transform player;
+    void Start()
+    {
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+        }
+        waypoints = new Transform[waypointParent.childCount];
+        for (int i = 0; i < waypointParent.childCount; i++)
+        {
+            waypoints[i] = waypointParent.GetChild(i);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (isWaiting)
+        {
+            return;
+        }
+
+        if (distanceToPlayer <= detectionRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            MoveToWaypoint(); 
+        }
+       
+    }
+
+    void MoveToWaypoint()
+    {
+        Transform target = waypoints[currentWaypointIndex];
+        transform.position = Vector2.MoveTowards(transform.position, target.position, movespeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, target.position) < 0.1f)
+        {
+            StartCoroutine(WaitAtWaypoint());
+        }
+    }
+
+    IEnumerator WaitAtWaypoint()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTime);
+
+        currentWaypointIndex = loopWaypoints ? (currentWaypointIndex + 1) % waypoints.Length : Mathf.Min(currentWaypointIndex + 1, waypoints.Length - 1);
+
+        isWaiting = false;
+    }
+
+    void ChasePlayer()
+    {
+        
+        transform.position = Vector2.MoveTowards(transform.position, player.position, movespeed * Time.deltaTime);
+
+    }
+
+}
+
+
+
