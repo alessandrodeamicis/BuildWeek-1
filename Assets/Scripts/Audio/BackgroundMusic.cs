ce43eb51936539791d3fc10a0e76d9ad8d9102be
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-06-12 21:52:49 +0200

    Major fixes

--- Assets/Scripts/Audio/BackgroundMusic.cs
+++ Assets/Scripts/Audio/BackgroundMusic.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BackgroundMusic : MonoBehaviour
 {
     public AudioClip musicClip;
-    [Range(0f, 1f)] public float volume = 0.5f;
+    [Range(0f, 1f)] public float volume = 0.3f;
     public bool persistOnLoad = true;
 
     private static AudioSource currentMusic;
