bc3e6963bf0f09f262501fc2638a01b000f51428
reru96 <renatorumieri96@outlook.com>
2025-06-12 18:34:18 +0200

    Fix Weapon rotating

--- Assets/Scripts/Weapon/WeaponRotatingDirection.cs
+++ Assets/Scripts/Weapon/WeaponRotatingDirection.cs
@@ -1,15 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponRotatingDirection : BaseWeapon
 {
-    private int currentIndex = 0;
-    private float[] angles = { 0f, 90f, 180f, 270f }; // Destra, Su, Sinistra, Giù
-
+    
     protected override void Fire()
     {
-        Instantiate(projectilePrefab, firePoint.position, Quaternion.Euler(0, 0, angles[currentIndex]));
-        currentIndex = (currentIndex + 1) % angles.Length;
+        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        GameObject scythe = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+        scythe.GetComponent<RotatingScythe>().firePoint = firePoint;
     }
 }
--- Assets/Scripts/Weapon/WeaponSideShooter.cs
+++ Assets/Scripts/Weapon/WeaponSideShooter.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class WeaponSideShooter : BaseWeapon
 {
+    public float spreadAngle = 180f;
+
+    // Start is called before the first frame update
+    protected Vector2 shootDirection = Vector2.right;
     protected override void Fire()
     {
-        Instantiate(projectilePrefab, firePoint.position, Quaternion.Euler(0, 0, 0));
-        Instantiate(projectilePrefab, firePoint.position, Quaternion.Euler(0, 0, 180));
+        UpdateShootDirection();
+        FireBullet(0f);
+        FireBullet(+spreadAngle);
+
+    }
+
+    void UpdateShootDirection()
+    {
+
+        shootDirection.x = Input.GetAxisRaw("Horizontal");
+        shootDirection.y = Input.GetAxisRaw("Vertical");
+
+
+        if (shootDirection.sqrMagnitude > 0.01f)
+        {
+            shootDirection.Normalize();
+        }
+        else
+        {
+            shootDirection = Vector2.right;
+        }
+    }
+
+    void FireBullet(float angleOffset)
+    {
+
+        Quaternion spreadRot = Quaternion.Euler(0, 0, angleOffset);
+        Vector2 finalDirection = spreadRot * shootDirection;
+        float angle = Mathf.Atan2(finalDirection.y, finalDirection.x) * Mathf.Rad2Deg;
+        Quaternion bulletRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+        Bullet bullet = Instantiate(projectilePrefab, firePoint.position, bulletRotation).GetComponent<Bullet>();
+        bullet.dir = finalDirection;
     }
 
 }
