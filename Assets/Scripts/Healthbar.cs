95ad5b25c2ed9b897d117d4332b7a016d1360882
reru96 <renatorumieri96@outlook.com>
2025-06-13 00:45:53 +0200

    terzo debug

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class Healthbar : MonoBehaviour
 {
-
+    LifeController life;
     public Slider slider;
-
     public void SetMaxHealth(int hp)
     {
         slider.maxValue = hp;
         slider.value = hp;
+
     }
 
     public void SetHealth(int hp)
     {
         slider.value = hp;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = GetComponent<LifeController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
